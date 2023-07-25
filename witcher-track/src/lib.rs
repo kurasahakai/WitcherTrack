926a8aad6ca7548b8d14a48f5395bc74fa7d1612
Andrea Venuta <venutawebdesign@gmail.com>
2023-07-25 10:06:32 +0200

    Add unsafes and comments

--- witcher-track/src/lib.rs
+++ witcher-track/src/lib.rs
@@ -11,11 +11,15 @@ use tesseract_sys::{
     TessBaseAPISetImage2, TessDeleteText,
 };
 
+mod data;
+mod screenshot;
+
 const ENG_TRAINEDDATA_URL: &str =
     "https://github.com/tesseract-ocr/tessdata/raw/main/eng.traineddata";
 
 const TRAINEDDATA_PATH: &str = "./eng.traineddata";
 
+/// Download english Tesseract trained data if it is not present.
 pub fn download_trained_data() -> Result<()> {
     if Path::new(TRAINEDDATA_PATH).exists() {
         return Ok(());
@@ -29,6 +33,7 @@ pub fn download_trained_data() -> Result<()> {
     Ok(())
 }
 
+/// RAII wrapper around Tesseract API
 pub struct OcrReader {
     handle: *mut TessBaseAPI,
 }
@@ -37,28 +42,30 @@ unsafe impl Send for OcrReader {}
 unsafe impl Sync for OcrReader {}
 
 impl OcrReader {
+    /// Construct a new instance.
     pub fn new() -> Result<Self> {
         let tessdata_path = CString::new(".").unwrap();
         let language = CString::new("eng").unwrap();
 
-        let handle = TessBaseAPICreate();
+        let handle = unsafe { TessBaseAPICreate() };
         unsafe { TessBaseAPIInit3(handle, tessdata_path.as_ptr(), language.as_ptr()) };
 
         Ok(Self { handle })
     }
 
+    /// Load an image from a
     pub fn get_ocr(&self, image_path: &Path) -> Result<String> {
         let image_path = CString::new(
             image_path
                 .to_str()
                 .ok_or_else(|| anyhow!("Could not convert path to string: {image_path:?}"))?,
         )?;
-        let image = pixRead(image_path.as_ptr());
+        let image = unsafe { pixRead(image_path.as_ptr()) };
 
         unsafe { TessBaseAPISetImage2(self.handle, image) };
 
         let text = unsafe { TessBaseAPIGetUTF8Text(self.handle) };
-        let text_str = CStr::from_ptr(text).to_string_lossy().into_owned();
+        let text_str = unsafe { CStr::from_ptr(text) }.to_string_lossy().into_owned();
 
         unsafe { TessDeleteText(text) };
 
