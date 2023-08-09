7232bc1b66f397655cc9aba6358e6e74fcd55e1e
Andrea Venuta <venutawebdesign@gmail.com>
2023-08-09 18:16:46 +0200

    C# (cringe)

---
+++ witcher-save-cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+using SaveFile;
+
+class Quest
+{
+    public string Guid { get; set; }
+    public string Status { get; set; }
+}
+
+public class Program
+{
+    static void Main(string[] Args)
+    {
+        var savefile = new Witcher3SaveFile("../witcher-save/QuickSave.sav", Witcher3ReadLevel.Quick);
+        var quests = savefile.CJournalManager.Statuses
+          .ConvertAll(status => new Quest { 
+              Guid = status.PrimaryGUID, 
+              Status = status.Status.ToString() 
+          });
+        string jsonData = JsonConvert.SerializeObject(quests, Formatting.Indented);
+        File.WriteAllText("save.json", jsonData);
+    }
+}
